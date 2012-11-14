a4d050297d64535204a9afdb25706a261dc9894c
Nirav Sanghani <nirav.sanghani@gmail.com>
2012-11-13 16:11:15 -0800

    Tweaks

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 // http://matt.might.net/articles/how-to-native-iphone-ipad-apps-in-javascript/
+// http://www.sitepen.com/blog/2011/12/07/touching-and-gesturing-on-iphone-android-and-more/
 
 $(function() {
 
   fixtures();
+
+  // Prevent elastic scrolling
+  $('body').on('touchmove', function(e) {
+    e.preventDefault();
+  });
 
   $('.transpose-down').click(function(e) {
     $chart.setKey($chart.key - 1);
